a9cfbd565b31aae0c12cdeb1f1ba7658a020f116
Aniara Zwarg <112500673+aniarazwarg@users.noreply.github.com>
2023-11-29 17:01:25 -0300

    tentando carrinho no banco

--- src/components/carrinho.jsx
+++ src/components/carrinho.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Button, Card, Col, Row } from "react-bootstrap";
+import { Button, Card, Col, Form, Row } from "react-bootstrap";
 
 function Cart({ cart, removeFromCart, sendOrder }) {
-  const getTotalPrice = () => {
+ const getTotalPrice = () => {
     if (Array.isArray(cart) && cart.length > 0) {
       const totalPrice = cart.reduce((total, item) => total + Number(item.preco), 0);
       return typeof totalPrice === "number" ? totalPrice.toFixed(2) : "0.00";
     } else {
       return "0.00";
     }
-  };
+ };
 
-  const handleBuyClick = () => {
+ const handleBuyClick = () => {
     if (cart.length > 0) {
       // Armazena o carrinho no localStorage antes de enviar o pedido
       localStorage.setItem('carrinho', JSON.stringify(cart));
@@ -19,15 +19,15 @@ function Cart({ cart, removeFromCart, sendOrder }) {
     } else {
       alert("Seu carrinho está vazio. Adicione itens antes de comprar.");
     }
-  };
+ };
 
-  const handleClearCart = () => {
+ const handleClearCart = () => {
     // Limpa o carrinho no localStorage e no estado local
     localStorage.removeItem('carrinho');
     removeFromCart(null, true); // Ajuste a função removeFromCart conforme necessário
-  };
+ };
 
-  return (
+ return (
     <Card style={{ width: "100%", marginBottom: 30 }}>
       <Card.Body>
         <Row>
@@ -35,14 +35,14 @@ function Cart({ cart, removeFromCart, sendOrder }) {
             <ul>
               {cart.map((item, index) => (
                 <li key={index}>
-                  {item.sabor} - R${item.preco}
-                  <Button
+                 {item.sabor} - R${item.preco}
+                 <Button
                     variant="outline-danger"
                     onClick={() => removeFromCart(item.id)}
                     style={{ marginLeft: 10 }}
-                  >
+                 >
                     Remover
-                  </Button>
+                 </Button>
                 </li>
               ))}
             </ul>
@@ -51,11 +51,21 @@ function Cart({ cart, removeFromCart, sendOrder }) {
             </Row>
           </Col>
           <Col md={6} className="d-flex align-items-center justify-content-center">
+            <Form>
+              <Form.Group controlId="formPaymentMethod">
+                <Form.Label>Forma de Pagamento</Form.Label>
+                <Form.Control as="select">
+                 <option>Dinheiro</option>
+                 <option>Cartão</option>
+                 <option>Pix</option>
+                </Form.Control>
+              </Form.Group>
+            </Form>
             <Button
               variant="success"
               onClick={handleBuyClick}
               disabled={cart.length === 0}
-              style={{ marginRight: 10 }}
+              style={{ marginRight: 10, marginLeft: 10 }}
             >
               Comprar
             </Button>
@@ -69,7 +79,7 @@ function Cart({ cart, removeFromCart, sendOrder }) {
         </Row>
       </Card.Body>
     </Card>
-  );
+ );
 }
 
 export default Cart;
